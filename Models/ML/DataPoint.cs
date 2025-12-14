09e7181ed8551c03528d0af3d1cbe38bc72f89f9
Ahmed Al Hamed <133318802+AHMEDMOM1@users.noreply.github.com>
2025-12-14 21:46:08 +0300

    Add files via upload

---
+++ Models/ML/DataPoint.cs
@@ -0,0 +1,9 @@
+﻿namespace WindowsFormsApp1.Models.ML
+{
+    public class DataPoint
+    {
+        public double Feature1 { get; set; }   
+        public double Feature2 { get; set; }   
+        public string Label { get; set; }      
+    }
+}
